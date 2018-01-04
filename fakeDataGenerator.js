04ab3f1bc236d30ebae40260c2014078235f563b
theoklitosBam7 <th.bampouris@gmail.com>
2018-01-04 14:00:13 +0200

    Add a for ... of loop with lodash

--- fakeDataGenerator.js
+++ fakeDataGenerator.js
@@ -1,11 +1,14 @@
 const faker = require('faker');
 const jsonfile = require('jsonfile');
+const _ = require('lodash');
 
 const file = 'db.json';
 const db = { movies: [] };
+const numbers = [..._.range(1, 51)];
 
-for (let i = 1; i <= 50; i += 1) {
+for (const number of numbers) {
   db.movies.push({
+    movieID: number,
     avatar: faker.image.image(),
     name: faker.random.words(),
     description: faker.lorem.paragraph(),
@@ -17,7 +20,7 @@ for (let i = 1; i <= 50; i += 1) {
 
 jsonfile.writeFile(file, db, { spaces: 2 }, (err) => {
   if (!err) {
-    console.log('Database file created successfully!')
+    console.log('Database file created successfully!');
   } else {
     console.error(err);
   }
